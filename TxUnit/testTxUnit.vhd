01d3efeb76eb5472b9f85510d0294d32e8a7da8a
hocinemediani <dolcejeannarciss@gmail.com>
2025-11-30 16:52:28 +0100

    ajout des tests txunit

--- TxUnit/testTxUnit.vhd
+++ TxUnit/testTxUnit.vhd
@@ -27,120 +27,137 @@
 --------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
+
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --USE ieee.numeric_std.ALL;
- 
+
 ENTITY testTxUnit IS
 END testTxUnit;
- 
+
 ARCHITECTURE behavior OF testTxUnit IS 
- 
-    -- Component Declaration for the Unit Under Test (UUT)
- 
-    COMPONENT TxUnit
-    PORT(
-         clk : IN  std_logic;
-         reset : IN  std_logic;
-         enable : IN  std_logic;
-         ld : IN  std_logic;
-         txd : OUT  std_logic;
-         regE : OUT  std_logic;
-         bufE : OUT  std_logic;
-         data : IN  std_logic_vector(7 downto 0)
-        );
-    END COMPONENT;
-   
-    COMPONENT clkUnit
-    PORT(
-         clk : IN  std_logic;
-         reset : IN  std_logic;
-         enableTX : OUT  std_logic;
-         enableRX : OUT  std_logic
-        );
-    END COMPONENT;	
 
-   signal clk : std_logic := '0';
-   signal reset : std_logic := '0';
-   signal enableTx : std_logic := '0';
-   signal enableRx : std_logic := '0';
-   signal ld : std_logic := '0';
-   signal data : std_logic_vector(7 downto 0) := (others => '0');
+  -- Component Declaration for the Unit Under Test (UUT)
 
-   signal txd : std_logic;
-   signal regE : std_logic;
-   signal bufE : std_logic;
+  COMPONENT TxUnit
+  PORT(
+        clk : IN  std_logic;
+        reset : IN  std_logic;
+        enable : IN  std_logic;
+        ld : IN  std_logic;
+        txd : OUT  std_logic;
+        regE : OUT  std_logic;
+        bufE : OUT  std_logic;
+        data : IN  std_logic_vector(7 downto 0)
+      );
+  END COMPONENT;
+  
+  COMPONENT clkUnit
+  PORT(
+        clk : IN  std_logic;
+        reset : IN  std_logic;
+        enableTX : OUT  std_logic;
+        enableRX : OUT  std_logic
+      );
+  END COMPONENT;	
 
-   -- Clock period definitions
-   constant clk_period : time := 10 ns;
- 
+  signal clk : std_logic := '0';
+  signal reset : std_logic := '0';
+  signal enableTx : std_logic := '0';
+  signal enableRx : std_logic := '0';
+  signal ld : std_logic := '0';
+  signal data : std_logic_vector(7 downto 0) := (others => '0');
+
+  signal txd : std_logic;
+  signal regE : std_logic;
+  signal bufE : std_logic;
+
+  -- Clock period definitions
+  constant clk_period : time := 10 ns;
+
 BEGIN
- 
-	-- Instantiate the Unit Under Test
-   uut0: TxUnit PORT MAP (
-          clk => clk,
-          reset => reset,
-          enable => enableTX,
-          ld => ld,
-          txd => txd,
-          regE => regE,
-          bufE => bufE,
-          data => data
-        );
 
-   -- Instantiate the clkUnit
-   clkUnit1: clkUnit PORT MAP (
-          clk => clk,
-          reset => reset,
-          enableTX => enableTX,
-          enableRX => enableRX
-        );
-		  
-   -- Clock process definitions
-   clk_process :process
-   begin
-     clk <= '0';
-     wait for clk_period/2;
-     clk <= '1';
-     wait for clk_period/2;
-   end process;
- 
- 
-   -- Stimulus process
-   stim_proc: process
-   begin		
-     -- maintien du reset durant 100 ns.
-     wait for 100 ns;	
-     reset <= '1';
+-- Instantiate the Unit Under Test
+  uut0: TxUnit PORT MAP (
+        clk => clk,
+        reset => reset,
+        enable => enableTX,
+        ld => ld,
+        txd => txd,
+        regE => regE,
+        bufE => bufE,
+        data => data
+      );
 
-     wait for 200 ns;
+  -- Instantiate the clkUnit
+  clkUnit1: clkUnit PORT MAP (
+        clk => clk,
+        reset => reset,
+        enableTX => enableTX,
+        enableRX => enableRX
+      );
+    
+  -- Clock process definitions
+  clk_process :process
+  begin
+    clk <= '0';
+    wait for clk_period/2;
+    clk <= '1';
+    wait for clk_period/2;
+  end process;
 
-     -- l'émetteur est dispo ?
-     if not (regE='1' and bufE='1') then
-       wait until regE='1' and bufE='1';
-     end if;
 
-     -- si oui, on charge la donnée
-     wait for clk_period;
-     -- émission du caractère 0x55
-     data <= "01010101";
-     ld <= '1';
+  -- Stimulus process
+  stim_proc: process
+  begin		
+    -- maintien du reset durant 100 ns.
+    wait for 100 ns;	
+    reset <= '1';
 
-     -- on attend de voir que l'ordre d'émission
-     -- a été bien pris en compte avant de rabaisser
-     -- le signal ld
-     if not (regE='1' and bufE='0') then
-       wait until regE='1' and bufE='0';
-     end if;
-     wait for clk_period;
-     ld <= '0';
-     
-     -- compléter avec des tests qui montrent que la prise en compte de la demande d'émission
-     -- d'un autre caractère se fait lorsqu'on émet un caractère et ceci quelque soit
-     -- l'étape d'émission
+    wait for 200 ns;
 
-     wait;
-   end process;
+    -- l'émetteur est dispo ?
+    if not (regE='1' and bufE='1') then
+      wait until regE='1' and bufE='1';
+    end if;
+
+    -- si oui, on charge la donnée
+    wait for clk_period;
+    -- émission du caractère 0x55
+    data <= "01010101";
+    ld <= '1';
+
+    -- on attend de voir que l'ordre d'émission
+    -- a été bien pris en compte avant de rabaisser
+    -- le signal ld
+    if not (regE='1' and bufE='0') then
+      wait until regE='1' and bufE='0';
+    end if;
+    wait for clk_period;
+    ld <= '0';
+    
+    -- compléter avec des tests qui montrent que la prise en compte de la demande d'émission
+    -- d'un autre caractère se fait lorsqu'on émet un caractère et ceci quelque soit
+    -- l'étape d'émission
+
+    wait for 200 ns;
+    -- émission du caractère 0x45 durant l'emission du caractere 0x55
+    data <= "01000101";
+    ld <= '1';
+    wait for clk_period;
+    -- emission du caractere 0x50 juste apres l'information d'emission du caractere
+    -- 0x45, elle ne sera pas prise en compte, le buffer de transmission etant deja
+    -- rempli, la transmission de 0x45 par contre continuera.
+    data <= "01010000";
+    wait for clk_period;
+    ld <= '0';
+    -- on a alors :
+      -- * emission de 0x55.
+      -- * prise en compte de la demande d'emission de 0x45 car le buffer a ete libere.
+      -- * demande d'emission de 0x50, non prise en compte car le buffer est plein a ce
+      -- moment la.
+      -- fin de la transmission avex txd qui repart a 1.
+    wait;
+  end process;
 
 END;
