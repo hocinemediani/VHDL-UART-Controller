f31a5c466a39b80c325aa0266a48650cf7bd7481
Aurelien VERBEKE <aurelien.verbeke@etu.toulouse-inp.fr>
2025-12-12 15:43:54 +0100

    implementation compteur16 and controleReception for RxUnit

--- UART/compteur16.vhd
+++ UART/compteur16.vhd
@@ -4,18 +4,57 @@ use IEEE.numeric_std.all;
 
 entity compteur16 is
   port (
-		clk 		: in std_logic;
-		enable 	: in std_logic;
-		reset 	: in std_logic;
-		RxD 		: in std_logic;
-		tmpClk 	: out std_logic;
-		tmpRxd 	: out std_logic);
+    clk 	: in std_logic;
+    enable 	: in std_logic;
+    reset 	: in std_logic;
+    RxD 	: in std_logic;
+    tmpClk 	: out std_logic;
+    tmpRxd 	: out std_logic);
 end compteur16;
 
 architecture behavioral of compteur16 is
-	
+  
+  type t_etat is (REPOS, COMPTAGE);
+
+  signal etat : t_etat := REPOS;
+  
+  
 begin
-	
-	
-	
+  
+  process (clk, reset) is
+
+    variable compteur : natural;
+
+  begin
+
+    if (reset = '0') then
+      tmpClk <= '1';
+      etat <= REPOS;
+      compteur := 8;
+      tmpRxd <= 'U';
+
+    else if (rising_edge(clk) and enable = '1') then
+      case etat is
+        when REPOS =>
+          if (RxD = '0') then
+            etat <= COMPTAGE;
+          end if;
+
+        when COMPTAGE =>
+          compteur := compteur - 1;
+          if (compteur = 0) then
+            compteur := 8;
+            if (tmpClk = '0') then
+              tmpRxd <= RxD;
+              tmpClk <= '1';
+            else
+              tmpClk <= '0';
+            end if;
+          end if;
+      end case;
+    end if;
+
+  end
+  
+        
 end behavioral;
--- UART/controleReception.vhd
+++ UART/controleReception.vhd
@@ -5,20 +5,92 @@ use IEEE.numeric_std.all;
 entity controleReception is
   port (
     clk 		: in std_logic;
-		reset 	: in std_logic;
-		read 		: in std_logic;
-		tmpClk 	: in std_logic;
-		tmpRxd 	: in std_logic;
-		FErr 		: out std_logic;
-		OErr 		: out std_logic;
-		DRdy 		: out std_logic;
-		data 		: out std_logic_vector(7 downto 0));
+    reset 	: in std_logic;
+    read 		: in std_logic;
+    tmpClk 	: in std_logic;
+    tmpRxd 	: in std_logic;
+    FErr 		: out std_logic;
+    OErr 		: out std_logic;
+    DRdy 		: out std_logic;
+    data 		: out std_logic_vector(7 downto 0));
 end controleReception;
 
 architecture behavioral of controleReception is
-    
+
+  signal buf : std_logic_vector(9 downto 0) := (others => 'U');
+
+  type t_etat is (REPOS, RECEPTION, ATTENTE);
+  signal etat : t_etat := REPOS;
+  
 begin
     
+    process (clk, reset) is
+
+      variable compteur : natural := 9;
+      variable compteur_read : natural := 5;
+
+    begin
+      
+      if (reset = '0') then
+        DRdy <= '0';
+        FErr <= '0';
+        OErr <= '0';
+        compteur := 9;
+        compteur_read := 5;
+        data <= (others => 'U');
+        etat <= REPOS;
+
+      else if (rising_edge(clk)) then
+        case etat is
+          when REPOS =>
+            if (tmpClk = '1' and tmpRxd = '0') then
+              DRdy <= '0';
+              FErr <= '0';
+              OErr <= '0';
+              data <= (others => 'U');
+              etat <= RECEPTION;
+              compteur := 9;
+            end if;
+
+          when RECEPTION =>
+            -- donnée positionnée sur tmpRxd au front montant de tmpClk
+            if (tmpClk = '1')
+              -- on la lit
+              buf(compteur) <= tmpRxd;
     
-    
+              if (compteur = 0) then
+                -- on est sur le bit de stop
+                
+                if (tmpRxd = '1') then
+                  -- bit de stop correct
+                  data <= buf(8 downto 1);
+                  compteur_read := 5;
+                  DRdy <= '1';
+                  FErr <= '0';
+                  OErr <= '0';
+                  etat <= ATTENTE;
+                
+                else
+                  -- bit de stop incorrect
+                  FErr <= '1';
+                end if;
+              
+              else
+                -- on est sur un autre bit : start ou donnée
+                compteur := compteur - 1;
+              end if;
+            end if;
+            
+          when ATTENTE =>
+            compteur_read := compteur_read - 1;
+            if (compteur_read = 0) then
+              OErr <= '1';
+              DRdy <= '0';
+            else if (read = '1') then
+              DRdy <= '0';
+              etat <= REPOS;
+            end if;
+        end case;
+      end if;
+    end process;
 end behavioral;
