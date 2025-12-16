1522707b45b03687c7ed71315b73a1ef5ca1e39e
hocinemediani <dolcejeannarciss@gmail.com>
2025-12-16 10:17:55 +0100

    added component declaration

--- UART/RxUnit.vhd
+++ UART/RxUnit.vhd
@@ -14,6 +14,29 @@ end RxUnit;
 architecture behavorial of RxUnit is
 
   signal tmpClk, tmpRxd : std_logic;
+  
+  component compteur16 is
+  port (
+    clk 	: in std_logic;
+    enable 	: in std_logic;
+    reset 	: in std_logic;
+    RxD 	: in std_logic;
+    tmpClk 	: out std_logic;
+    tmpRxd 	: out std_logic);
+  end component;
+  
+  component controleReception is
+  port (
+    clk 		: in std_logic;
+    reset 	    : in std_logic;
+    read 		: in std_logic;
+    tmpClk 	    : in std_logic;
+    tmpRxd 	    : in std_logic;
+    FErr 		: out std_logic;
+    OErr 		: out std_logic;
+    DRdy 		: out std_logic;
+    data 		: out std_logic_vector(7 downto 0));
+  end component;
 
 begin
 
